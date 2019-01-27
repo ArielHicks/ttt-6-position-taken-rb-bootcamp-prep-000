79158468f486fd6016078c9387cf0f12fd5f4a3c
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-27 19:08:39 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
     if board == " "
-    else false
-    # elsif board == "X" && "O"
-    # else true
+    elsif false
+    elsif index == "X" || "O"
+    else true
   end
 end
